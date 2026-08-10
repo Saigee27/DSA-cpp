1f8a577739decb1c791db7470317eb28632307e6
Jayant <saigee2703@gmail.com>
2026-08-10 23:07:36 +0530

    feat: add bubble sort

---
+++ sorting/bubblesort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+int main()
+{
+    int arr[10]= {6, 2, 9, 1, 5, 11, 10, 3, 15, 13};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int j=0; j<n-1; j++)
+    {
+        for (int i=0; i<n-1; i++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+            
+        }
+
+    }
+    
+    for (int i=0; i<n; i++)
+    {
+       std::cout<<arr[i]<<" "; 
+    }
+}
+
