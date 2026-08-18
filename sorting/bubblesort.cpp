13d99c41e6677568e41b52e34ec0b94799b0893e
Jayant <saigee2703@gmail.com>
2026-08-18 23:03:13 +0530

    Optimize bubble sort comparisons

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -26,15 +26,15 @@ int main()
     int arr[10]= {6, 2, 9, 1, 5, 11, 10, 3, 15, 13};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    for(int j=0; j<n-1; j++)
+    for(int i=0; i<n-1; i++)
     {
-        for (int i=0; i<n-1; i++)
+        for (int j=0; j<n-i-1; j++)
         {
-            if (arr[i] > arr[i+1])
+            if (arr[j] > arr[j+1])
             {
-                int temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
             
         }
